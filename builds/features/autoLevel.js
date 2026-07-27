caff6431fb192f28d203de6f4980b26d37ac84b0
JS <justas.stasiulaitis@yahoo.com>
2026-07-27 14:23:30 +0300

    Update comment for M48 test in autoLevel.js

--- builds/features/autoLevel.js
+++ builds/features/autoLevel.js
@@ -11,7 +11,7 @@ module.exports = {
             "LCD_BED_LEVELING",
             ["DEFAULT_LEVELING_FADE_HEIGHT", 20],
             ["NOZZLE_TO_PROBE_OFFSET", [0, 0, 0]],
-            //M48 test trigger rebuild
+            //M48 test
             "Z_MIN_PROBE_REPEATABILITY_TEST"
         ],
         disable: [
